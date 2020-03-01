b4ebb78cc5c1a6ea145c64696b65756a864006e1
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-02-29 17:58:32 -0800

    add counting status msg

--- static/main.js
+++ static/main.js
@@ -726,7 +726,7 @@ function main( JGO, axutil, p_options) {
   //-----------------------------------------------------
   function get_prob( completion, update_emo, playing) {
     if (activate_bot.state == 'on') {
-      $('#status').html( 'KataGo is thinking...')
+      $('#status').html( 'KataGo is counting...')
     }
     else {
       $('#status').html( '...')
