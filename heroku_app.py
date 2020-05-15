bcd8728e046eed5d41cf68340837b2fe4a381bb0
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-05-15 11:50:42 -0700

    get_sgf_tag() bug it tag does not exist

--- heroku_app.py
+++ heroku_app.py
@@ -92,6 +92,7 @@ def slog():
 def get_sgf_tag( sgfstr, tag):
     rexp = r'.*' + tag + r'\[([^\[]*)\].*'
     res = re.sub(rexp, r'\1', sgfstr.decode('utf8'),flags=re.DOTALL)
+    if res == tstr: return '' # tag not found
     return res
 
 @app.route('/sgf2list', methods=['POST'])
