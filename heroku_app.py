5e17449e572b70d69b7edec3aae26922aea0eb36
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-06-02 10:17:56 -0700

    try http for back end

--- heroku_app.py
+++ heroku_app.py
@@ -38,7 +38,8 @@ app.config.update(
 app.config['DEBUG'] = os.getenv("DEBUG", False)
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 
-KATAGO_SERVER = 'https://ahaux.com/katago_server/'
+# KATAGO_SERVER = 'https://ahaux.com/katago_server/'
+KATAGO_SERVER = 'http://www.ahaux.com/katago_server/'
 
 #--------------
 # Endpoints
