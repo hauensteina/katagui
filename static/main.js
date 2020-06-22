9d09be33ebfd6217758fac954db86aeb1f71c3f3
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-06-22 15:21:13 -0700

    disable strong

--- static/main.js
+++ static/main.js
@@ -1104,7 +1104,7 @@ function main( JGO, axutil, p_options) {
     }
     // setter
     if (val == 'strong') {
-      const STRONG = 1
+      const STRONG = 0
       if (STRONG) {
          $('#descr_bot').html( 'KataGo 40b 1000<br>2020-06-22')
          $('#btn_strong').addClass('active')
