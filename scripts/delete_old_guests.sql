979b202d9588a2c6d6779705e1239aa553d0905b
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-08-01 17:37:59 -0700

    move the new game button down in mobile version

--- scripts/delete_old_guests.sql
+++ scripts/delete_old_guests.sql
@@ -1,2 +1,4 @@
 
-delete from t_user where username like 'guest_%' and extract(hour from now() - ts_last_seen)  > 12;
+delete from t_user where username like 'guest_%' and extract(epoch from now() - ts_last_seen)/3600  > 12;
+
+
