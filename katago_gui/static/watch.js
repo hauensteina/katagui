1382b4585f5901943a61761c5ee0da96abddc6f6
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-09-25 15:54:34 -0700

    more watch time race nightmares

--- katago_gui/static/watch.js
+++ katago_gui/static/watch.js
@@ -964,23 +964,27 @@ function watch( JGO, axutil, game_hash, p_options) {
 
   //---------------------------------------------------------------------
   function update_game( action, game_hash, nmoves, client_timestamp) {
+    console.log( 'action: ' + action)
     if (toggle_live_button() == 'off') { return }
-    if (update_game.timer == '') {
-      update_game.timer = setTimeout( ()=>{ update_game( 'replay_with_delay') }, 1000)
-    }
+    // if (update_game.timer == '') {
+    //   update_game.timer = setInterval( ()=>{ update_game( 'replay_with_delay') }, 1000)
+    // }
     if (action == 'update_game') {
+      console.log( 'update game nmoves ' + nmoves + ' pos ' + update_game.pos)
       if (client_timestamp < update_game.timestamp) {
         console.log( 'update_game(): outdated update message, ignored')
         return
       }
       update_game.timestamp = client_timestamp
-      if (nmoves - update_game.pos > 3) {
-          //console.log('reset forwards')
-          update_game.pos = nmoves - 1
+      if (nmoves - update_game.pos > 5) {
+          console.log('reset forwards')
+        update_game.pos = nmoves - 1
+        update_game.gamerecords = []
       }
       else if (update_game.pos > nmoves - 1) {
-        //console.log('reset backwards')
+        console.log('reset backwards')
         update_game.pos = nmoves - 1
+        update_game.gamerecords = []
       }
 
 
@@ -988,29 +992,32 @@ function watch( JGO, axutil, game_hash, p_options) {
                                   (resp) => {
                                     var gr = new GameRecord()
                                     gr.from_dict( resp)
-                                    //console.log( 'diff:' + (nmoves - update_game.pos) + ' mc:' + (update_game.pos+1) + ' nm:' + nmoves)
+                                    console.log( 'diff:' + (nmoves - update_game.pos) + ' mc:' + (update_game.pos+1) + ' nm:' + nmoves)
                                     for (var i = update_game.pos+1; i <= nmoves; i++) {
 				      gr.seek( i)
 				      update_game.gamerecords.push( gr.clone())
+                                      console.log( 'pushing move ' + gr.pos())
                                     }
+                                    update_game.pos = nmoves
                                   })
     }
     // Place new stones with delay between moves for easier watching
     else if (action == 'replay_with_delay') {
-      clearTimeout( update_game.timer)
-      update_game.timer = setTimeout( ()=>{ update_game( 'replay_with_delay') }, 1000)
+      //clearTimeout( update_game.timer)
+      //update_game.timer = setTimeout( ()=>{ update_game( 'replay_with_delay') }, 1000)
       if (update_game.gamerecords.length == 0) {
 	return
       }
       var gr = update_game.gamerecords.shift()
       grec = gr.clone()
       goto_move( grec.pos())
-      update_game.pos = grec.pos()
+      console.log( 'replay move ' + grec.pos())
+      //update_game.pos = grec.pos()
       const update_emo = true; show_prob( update_emo)
     } // replay_with_delay
   } // update_game()
   update_game.gamerecords = []
-  update_game.timer = ''
+  update_game.timer = setInterval( ()=>{ update_game( 'replay_with_delay') }, 1000)
   update_game.timestamp = 0
   update_game.pos = 0
 
