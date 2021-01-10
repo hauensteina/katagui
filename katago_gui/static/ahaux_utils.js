8f279bf60519717aa5a7154441a09e3474b678f1
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2021-01-10 14:00:42 -0800

    Satoshi Banya 130 dollars

--- katago_gui/static/ahaux_utils.js
+++ katago_gui/static/ahaux_utils.js
@@ -5,7 +5,7 @@
 
 'use strict'
 
-const DDATE = '2020-12-23'
+const DDATE = '2021-01-10'
 const VERSION = '3.4.9'
 
 const COLNAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
