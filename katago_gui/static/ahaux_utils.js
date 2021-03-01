9630cecbc558367a62e98d1c42f6378297951246
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2021-02-28 17:58:16 -0800

    Sturat Barthropp 15 dollars

--- katago_gui/static/ahaux_utils.js
+++ katago_gui/static/ahaux_utils.js
@@ -5,7 +5,7 @@
 
 'use strict'
 
-const DDATE = '2021-02-19'
+const DDATE = '2021-02-28'
 const VERSION = '3.5.2'
 
 const COLNAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
