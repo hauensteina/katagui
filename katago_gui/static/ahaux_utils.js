9c1cb2bb1e3fbb504059ec33df62904ac0fa09a5
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2021-03-31 12:34:06 -0700

    remove date

--- katago_gui/static/ahaux_utils.js
+++ katago_gui/static/ahaux_utils.js
@@ -5,7 +5,7 @@
 
 'use strict'
 
-const DDATE = '2021-03-31'
+const DDATE = ''
 const VERSION = '3.6.1'
 
 const COLNAMES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
